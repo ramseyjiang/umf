7f6471d9f409d240fa4756b94102fbaa92631e9f
姜大巍 <davy820ramsey@gmail.com>
2020-04-23 10:37:36 +1200

    Updates

---
+++ precache-manifest.f5a5af2d0a7a6baaa0737b928dcdada8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a8c31b9a439889ef4bcfba72032af5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a06cf1eeb7168140e50e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "03b33083e8ff37365ae6",
+    "url": "/static/css/main.ae62f795.chunk.css"
+  },
+  {
+    "revision": "a06cf1eeb7168140e50e",
+    "url": "/static/js/2.9a2627eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a2627eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7f03e88fb5e59578cb",
+    "url": "/static/js/3.bf91130b.chunk.js"
+  },
+  {
+    "revision": "228e00a16b23fe815450",
+    "url": "/static/js/4.cdc10be4.chunk.js"
+  },
+  {
+    "revision": "e9d4ac23f9345debadd7",
+    "url": "/static/js/5.2a7bb452.chunk.js"
+  },
+  {
+    "revision": "03b33083e8ff37365ae6",
+    "url": "/static/js/main.0891d458.chunk.js"
+  },
+  {
+    "revision": "91720577b4676b5da958",
+    "url": "/static/js/runtime-main.9061ce94.js"
+  }
+]);
