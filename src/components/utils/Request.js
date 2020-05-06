5b4532aaed1823ebcc78a67257dd8a02b3e2c08f
姜大巍 <davy820ramsey@gmail.com>
2020-05-06 19:17:37 +1200

    update get request method

--- src/components/utils/Request.js
+++ src/components/utils/Request.js
@@ -2,7 +2,14 @@ import { getLocal } from "./Storage.js";
 
 export const get = async (url) => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: "get",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getLocal("token")}`,
+      },
+    });
     return await response.json();
   } catch (err) {
     console.log(err);
