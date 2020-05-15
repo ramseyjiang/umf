0b4e64496e46492c4cbf29cb447a6b7a6753123c
姜大巍 <davy820ramsey@gmail.com>
2020-05-15 12:20:47 +1200

    update format size and foot

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {day, month, year} from "../utils/Date.js"
+import {year} from "../utils/Date.js"
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
     return (
       <div className="font-small bg-dark" >
         <div className="text-center text-white py-1">
-        © {day()}/{month()}/{year()} Copyright:<label htmlFor="footer">Ramsey Jiang</label> {' '}
+        © {year()} Copyright:<label htmlFor="footer">Ramsey Jiang</label> {' '}
         <a href="https://github.com/ramseyjiang/"rel="noopener noreferrer" target="_blank" className="link-icon-color"><FaGithub /></a> {' '}
         <a href="https://www.linkedin.com/in/ramsey-jiang-360950101" target="_blank" rel="noopener noreferrer" className="link-icon-color"><FaLinkedin /></a>
         </div>
